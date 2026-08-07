e4bf11ab2467cb747da2a8fc679bbeb4a179f048
ndimamhangwani <ndima.mhangwani@techbridlefoundation.org>
2026-08-07 11:40:18 +0200

    feat: add application entry point (Main.java)

---
+++ src/main/java/com/library/Main.java
@@ -0,0 +1,19 @@
+package com.library;
+
+import com.library.ui.MainFrame;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ignored) {
+            // fall back to default look and feel
+        }
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+    }
+}
